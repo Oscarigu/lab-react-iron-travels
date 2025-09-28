25fd274527945debd14a328136e75db1ca4e123e
oscarigu <38469752+Oscarigu@users.noreply.github.com>
2025-09-28 20:05:13 +0200

    falta iteracion 3

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -2,19 +2,50 @@ import travelPlansData from "../assets/travel-plans.json";
 import { useState } from "react";
 function TravelList() {
   const [travels, setTravelsToDisplay] = useState(travelPlansData);
-
-  return(
+function update(){
+  
+}
+  return (
     <>
-  {travels.map((travelObj, i, arr) => {
-      return <div key= {travelObj.id} className="travel">
-        <img src={travelObj.image}></img>
-        <h2>{travelObj.destination}, ({travelObj.days} Days)</h2>
-        <p>{travelObj.description}</p>
-        <p>Price: {travelObj.totalCost}</p>
-      </div>;
-    })
-  }
-  </>
-)}
+      {travels.map((travelObj, i, arr) => {
+      let caja;
+      let inclusive;
+        if(travelObj.totalCost<=350){
+          caja = <div className="Greatdeal"> Great Deal </div>;
+        }
+       else if(travelObj.totalCost >= 1500){
+          caja = <div className="Premium"> Premium </div>;
+        }
+        else{
+          caja =<></>;
+        }
+        if(travelObj.allInclusive){
+          inclusive = <div className="AllInclusive">All-Inclusive</div>
+        }
+        else{
+          inclusive = <></>
+        }
+        return (
+          <div key={travelObj.id} className="travel">
+            <div className="imagen">
+              <img src={travelObj.image}></img>
+            </div>
+            <div className="texto">
+              <h2>
+                {travelObj.destination}, ({travelObj.days} Days)
+              </h2>
+              <p>{travelObj.description}</p>
+              <p>Price: {travelObj.totalCost}€</p>
+              {caja}
+              {inclusive}
+
+              <button className="dltbutton"> Delete </button>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
 
 export default TravelList;
